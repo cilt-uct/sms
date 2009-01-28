b8ffd5d5a569be0beebe37d678286d3ed8d1b880
wilhelm <wilhelm@psybergate.co.za>
2009-01-28 13:44:45 +0000

    SMS-3 missing content

---
+++ tool/src/webapp/content/js/searchCriteria.js
@@ -0,0 +1,34 @@
+function getToday(){
+	var now = new Date();
+	var y = now.getFullYear();
+	var m = now.getMonth() + 1;
+	var d = now.getDate();
+	var dateString = m + '/' + d + '/' + y; 
+	return dateString;
+}
+
+function checkNumbers(){
+	
+	var input = document.getElementById('searchCriteria::id');	
+	if(document.getElementById('searchCriteria::label-id').innerHTML == 'Account Number:'){
+		input.value = input.value.replace(/[^0-9]/g, '');
+	}
+	else{
+		input.value = input.value.replace(/[^0-9/+]/g, '');
+	}
+}
+
+//delete when deployed into sakai since this function will be available
+function openWindow(url, title, options)
+{
+	var win = top.window.open(url, title, options);
+	win.focus();
+	return win;
+}
+
+
+function make_task_row_open_popup(row) {
+		
+	var link = document.getElementById(row.id + 'link');
+	openWindow(link.href, 'SMSTask', 'resizable=yes,toolbar=no,scrollbars=yes,menubar=yes,width=460,height=500'); 
+}
---
+++ tool/src/webapp/content/js/sms.js
@@ -0,0 +1,4 @@
+function make_button_call_link(link_id) {
+		var link = document.getElementById(link_id);
+		window.location.href = link.href;
+}
