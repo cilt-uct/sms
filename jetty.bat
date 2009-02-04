eff68a39f34821bd6641222cd47af3bf2ecf7a6d
wilhelm <wilhelm@psybergate.co.za>
2009-02-04 06:58:25 +0000

    SMS-8 Copy test components (with ExternalLogicStub) to run with jetty

--- jetty.bat
+++ jetty.bat
@@ -1,3 +1,5 @@
+copy impl\target\test-classes\org tool\target\sms_ui\WEB-INF\classes
 cd tool
 copy test-resources\web.xml target\sms_ui\WEB-INF\web.xml /y
+copy test-resources\components.xml target\sms_ui\WEB-INF\components.xml /y 
 mvn -o -Dmaven.test.skip=true jetty:run-exploded
--- jetty.sh
+++ jetty.sh
@@ -1,3 +1,5 @@
+cp -r impl/target/test-classes/org tool/target/sms_ui/WEB-INF/classes
 cd tool
-cp test-resources/web.xml target/sms_ui/WEB-INF/web.xml 
+cp test-resources/web.xml target/sms_ui/WEB-INF/web.xml
+cp test-resources/components.xml target/sms_ui/WEB-INF/components.xml 
 mvn -o -Dmaven.test.skip=true jetty:run-exploded
