324f1f4f6374daf864fec0e9d842dbc9efb037be
Etienne <etienne@psybergate.co.za>
2009-02-04 11:37:36 +0000

    sms:7 added debug scripts

--- jetty_debug.sh
+++ jetty_debug.sh
@@ -1,2 +1,5 @@
+cp -r impl/target/test-classes/org tool/target/sms_ui/WEB-INF/classes
 cd tool
+cp test-resources/web.xml target/sms_ui/WEB-INF/web.xml
+cp test-resources/components.xml target/sms_ui/WEB-INF/components.xml
 mvnDebug -o -Dmaven.test.skip=true jetty:run-war
