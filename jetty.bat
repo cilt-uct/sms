cb88b7cd9647afe37c0d44c546937fb7e1d67b46
Louis <louis@psybergate.co.za>
2009-02-04 12:38:49 +0000

    fix problem with copy of test files

--- jetty.bat
+++ jetty.bat
@@ -1,4 +1,4 @@
-copy impl\target\test-classes\org tool\target\sms_ui\WEB-INF\classes
+xcopy impl\target\test-classes\org\sakaiproject\sms\*   tool\target\sms_ui\WEB-INF\classes\org\sakaiproject\sms\* /e /y
 cd tool
 copy test-resources\web.xml target\sms_ui\WEB-INF\web.xml /y
 copy test-resources\components.xml target\sms_ui\WEB-INF\components.xml /y 
