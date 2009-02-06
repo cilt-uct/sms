a7cb875fed8d0d0a2d66bf7e08cfeb0cb1e0a720
wilhelm <wilhelm@psybergate.co.za>
2009-02-06 05:55:26 +0000

    SMS-3 Fix CSV export javascript call Bug 232

--- tool/src/webapp/content/js/sms.js
+++ tool/src/webapp/content/js/sms.js
@@ -1,9 +1,10 @@
-function make_button_call_link(link_id) {
+// Calls a link (used for onclick)
+function call_link(link_id) {
 		var link = document.getElementById(link_id);
 		window.location.href = link.href;
 }
 
-// Makes a button call a link
+// Makes a button call a link (used for InitBlocks)
 function make_button_call_link(button_id, link_id) {
 	var button = document.getElementById(button_id);
 	var link = document.getElementById(link_id);
