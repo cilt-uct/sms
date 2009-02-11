6e58a87929ef07a3accb94e7a79c9199ba2ec109
Louis <louis@psybergate.co.za>
2009-02-11 08:53:51 +0000

    jetty batch file change

--- jetty.bat
+++ jetty.bat
@@ -1,11 +1,13 @@
+@echo off
+
 xcopy stubs\target\classes\org\sakaiproject\sms\*   tool\target\sms_ui\WEB-INF\classes\org\sakaiproject\sms\* /e /y
 cd tool
 
 rem make sure the new xml files is newer that that in the jar, also they will be overwritten
-touch test-resources\*.xml
+touch test-resources\web.xml
+touch test-resources\components.xml
 
 copy test-resources\web.xml target\sms_ui\WEB-INF\web.xml /y
 copy test-resources\components.xml target\sms_ui\WEB-INF\components.xml /y 
-
 
 mvn -o -Dmaven.test.skip=true jetty:run-exploded
