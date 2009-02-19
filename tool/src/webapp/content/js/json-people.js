9243541c6490f35953f0aff42b3f50af4294ae18
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-02-19 07:32:23 +0000

    SMS-10. Jsons for testing

---
+++ tool/src/webapp/content/js/json-people.js
@@ -0,0 +1,51 @@
+(
+{
+    "title": "d'bug",
+    "link": "http://blog.reindel.com",
+    "description": "d'bug shares tips, tricks, and advice with Web development professionals",
+    "published": "Thu, 02 Oct 2007 12:00:00 +0000",
+    "language": "en",
+    "people": [{
+        "roles": [{
+            "id": "3635463tv3t",
+            "name": "students"
+        },
+            {
+                "id": "36354546445663tv3t",
+                "name": "tutors"
+            },
+            {
+                "id": "3635463tv3t",
+                "name": "Lecturers"
+            },
+            {
+                "id": "3635463tv3t",
+                "name": "Site Owners"
+            }
+        ],
+
+        "groups": [{
+            "id": "3635463tv3t",
+            "name": "Tuts 1"
+        },
+            {
+                "id": "36354546445663tv3t",
+                "name": "Tts 2"
+            }]
+        ,
+
+        "participants": [{
+            "id": "3635463tv3t",
+            "name": "Tuts 1"
+        },
+            {
+                "id": "36354546445663tv3t",
+                "name": "Tts 2"
+            }]
+
+
+
+    }]
+}
+
+        )
