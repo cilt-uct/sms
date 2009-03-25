e88132a8313b7b539fcab2e5a0a2c7cf8f38b8a1
wilhelm <wilhelm@psybergate.co.za>
2009-03-25 13:17:20 +0000

    SMS-3 CSV reflector escape issue fix

--- tool/src/java/org/sakaiproject/sms/tool/util/BeanToCSVReflector.java
+++ tool/src/java/org/sakaiproject/sms/tool/util/BeanToCSVReflector.java
@@ -23,12 +23,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
- * A Utility class to convert a List of java beans to a CSV list, does not fetch nested objects
- *
+ * A Utility class to convert a List of java beans to a CSV list, does not fetch
+ * nested objects
+ * 
  */
 public class BeanToCSVReflector {
-
 
 	private static final String NOT_FOUND = "N/A";
 	private static final String IS = "is";
@@ -36,132 +38,143 @@ public class BeanToCSVReflector {
 
 	/**
 	 * Returns a CSV list of all the java accessors
-	 *  
-	 * @param list ist a list of objects
+	 * 
+	 * @param list
+	 *            ist a list of objects
 	 * @return a string containing a CSV report
 	 */
 	public String toCSV(List<?> list) {
-		return toCSV(list, (String[])null);
+		return toCSV(list, (String[]) null);
 	}
-	
+
 	/**
 	 * Returns a CSV list of only the specified field
 	 * 
-	 * @param list a list of objects
-	 * @param fieldNames the Fields to reflect on
+	 * @param list
+	 *            a list of objects
+	 * @param fieldNames
+	 *            the Fields to reflect on
 	 * @return a string containing a CSV report
 	 */
 	public String toCSV(List<?> list, String[] fieldNames) {
-		
+
 		StringBuffer buffer = new StringBuffer();
-		
-		if(list.size() == 0){
+
+		if (list.size() == 0) {
 			return "There is no data for this report";
 		}
-		
-		Object objectToReflectOn = list.get(0);
-		Method[] methodArray =  getAllPublicAccesorMethods(objectToReflectOn);
-		methodArray = createOrderedMethodArray(methodArray, fieldNames);
-		
-		for (int i = 0; i < methodArray.length; i++) {
-			String fieldName = convertMethodNameToFieldName(methodArray[i].getName());
 
-			if(i == methodArray.length - 1){
+		Object objectToReflectOn = list.get(0);
+		Method[] methodArray = getAllPublicAccesorMethods(objectToReflectOn);
+		methodArray = createOrderedMethodArray(methodArray, fieldNames);
+
+		for (int i = 0; i < methodArray.length; i++) {
+			String fieldName = convertMethodNameToFieldName(methodArray[i]
+					.getName());
+
+			if (i == methodArray.length - 1) {
 				buffer.append(fieldName + "\n");
-			}
-			else{
+			} else {
 				buffer.append(fieldName + ", ");
 			}
 		}
-		
-		
+
 		for (Object object : list) {
 			for (int i = 0; i < methodArray.length; i++) {
 				try {
 					String value = NOT_FOUND;
 					Object fieldValue = methodArray[i].invoke(object);
-					
-					if(fieldValue != null)
+
+					if (fieldValue != null) {
 						value = fieldValue.toString();
-						
-					if(i == methodArray.length - 1){
+						String escape = value.contains(",") ? "\"" : "";
+						value = escape
+								+ StringUtils.replace(value, "\"", "\"\"")
+								+ escape;
+					}
+					if (i == methodArray.length - 1) {
 						buffer.append(value + "\n");
+					} else {
+						buffer.append(value + ",");
 					}
-					else{
-						buffer.append(value + ", ");
-					}
-					
+
 				} catch (Exception e) {
-					throw new RuntimeException("Failed to obtain value from method "  + methodArray[i].toString() + " cause " + e.toString());
-				} 
+					throw new RuntimeException(
+							"Failed to obtain value from method "
+									+ methodArray[i].toString() + " cause "
+									+ e.toString());
+				}
 			}
 		}
-			
+
 		return buffer.toString();
 	}
 
 	private Method[] getAllPublicAccesorMethods(Object objectToReflectOn) {
-		
+
 		Method[] allMethods = objectToReflectOn.getClass().getMethods();
 		ArrayList<Method> accesorMethods = new ArrayList<Method>();
 
 		for (int i = 0; i < allMethods.length; i++) {
-			
+
 			String methodName = allMethods[i].getName();
-			
-			if(methodName.startsWith(GET))
+
+			if (methodName.startsWith(GET))
 				accesorMethods.add(allMethods[i]);
-			if(methodName.startsWith(IS))
+			if (methodName.startsWith(IS))
 				accesorMethods.add(allMethods[i]);
 		}
-		
+
 		Method[] methodList = new Method[accesorMethods.size()];
-		
-		for(int i = 0 ; i < methodList.length ; i++){
+
+		for (int i = 0; i < methodList.length; i++) {
 			methodList[i] = accesorMethods.get(i);
 		}
-		
+
 		return methodList;
 	}
 
-	private Method[] createOrderedMethodArray(Method[] methodArray, String[] fieldNames) {
-		
-		if(fieldNames == null){
+	private Method[] createOrderedMethodArray(Method[] methodArray,
+			String[] fieldNames) {
+
+		if (fieldNames == null) {
 			return methodArray;
 		}
-		
+
 		Map<String, Method> methodMap = new TreeMap<String, Method>();
 		for (int i = 0; i < methodArray.length; i++) {
-			
+
 			String methodName = methodArray[i].getName();
-			String fieldName = convertMethodNameToFieldName(methodName);			
+			String fieldName = convertMethodNameToFieldName(methodName);
 			methodMap.put(fieldName, methodArray[i]);
 		}
-		
+
 		Method[] methodsToInvoke = new Method[fieldNames.length];
 		for (int i = 0; i < fieldNames.length; i++) {
-			
+
 			Method tempMethod = methodMap.get(fieldNames[i]);
-			
-			if(tempMethod == null){
-				throw new RuntimeException("The Field value [" + fieldNames[i]  + "] does not exsist in the supplied Object List");
+
+			if (tempMethod == null) {
+				throw new RuntimeException("The Field value [" + fieldNames[i]
+						+ "] does not exsist in the supplied Object List");
 			}
-			
+
 			methodsToInvoke[i] = tempMethod;
 		}
-		
+
 		return methodsToInvoke;
 	}
 
 	private String convertMethodNameToFieldName(String methodName) {
-		//remove "get" or "is"
-		if(methodName.startsWith(GET) && methodName.length() > 0)
+		// remove "get" or "is"
+		if (methodName.startsWith(GET) && methodName.length() > 0)
 			methodName = methodName.substring(3);
-		if(methodName.startsWith(IS)  && methodName.length() > 0)
+		if (methodName.startsWith(IS) && methodName.length() > 0)
 			methodName = methodName.substring(2);
-		
-		if(methodName.length() >= 2)
-			methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
+
+		if (methodName.length() >= 2)
+			methodName = methodName.substring(0, 1).toLowerCase()
+					+ methodName.substring(1);
 		else
 			methodName = methodName.toLowerCase();
 		return methodName;
--- tool/src/test/org/sakaiproject/sms/tool/util/BeanToCSVReflectorTest.java
+++ tool/src/test/org/sakaiproject/sms/tool/util/BeanToCSVReflectorTest.java
@@ -23,10 +23,10 @@ import java.util.StringTokenizer;
 
 import junit.framework.TestCase;
 
-public class BeanToCSVReflectorTest extends TestCase{
+public class BeanToCSVReflectorTest extends TestCase {
 
 	private BeanToCSVReflector beanToCSVReflector;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -39,11 +39,11 @@ public class BeanToCSVReflectorTest extends TestCase{
 	}
 
 	public void testSimpleResultSetNoOrder() throws Exception {
-		
+
 		List<SimpleResultSet> list = createSimpleTestRow();
-		
+
 		String csvHeaderAndRow = beanToCSVReflector.toCSV(list);
-				
+
 		assertTrue(csvHeaderAndRow.contains(","));
 		assertTrue(csvHeaderAndRow.contains("label"));
 		assertTrue(csvHeaderAndRow.contains("Row 1"));
@@ -53,55 +53,58 @@ public class BeanToCSVReflectorTest extends TestCase{
 
 	public void testSimpleResultSetWithOrder() throws Exception {
 		List<SimpleResultSet> list = createSimpleTestRow();
-		String[] fieldNames = new String []{ "longValue", "label"};
-		
-		String csvHeaderAndRow = beanToCSVReflector.toCSV(list, fieldNames);		
+		String[] fieldNames = new String[] { "longValue", "label" };
+
+		String csvHeaderAndRow = beanToCSVReflector.toCSV(list, fieldNames);
 		assertTrue(csvHeaderAndRow.length() > 0);
-		
-		StringTokenizer stringTokenizer = new StringTokenizer(csvHeaderAndRow, "\n");
-		
+
+		StringTokenizer stringTokenizer = new StringTokenizer(csvHeaderAndRow,
+				"\n");
+
 		assertEquals("longValue, label", stringTokenizer.nextElement());
-		assertEquals("2000, Row 1", stringTokenizer.nextElement());
+		assertEquals("2000,Row 1", stringTokenizer.nextElement());
 	}
-	
+
 	public void testExtendedResultNoOrder() throws Exception {
-		
+
 		List<ExtendedResultSet> list = createExtendedTestRow();
-		String extendedCsvHeaderAndRow  = beanToCSVReflector.toCSV(list);
-		
+		String extendedCsvHeaderAndRow = beanToCSVReflector.toCSV(list);
+
 		assertTrue(extendedCsvHeaderAndRow.contains("Row 1"));
 		assertTrue(extendedCsvHeaderAndRow.contains("More info"));
 	}
-	
-	public void testResultSetWithBoolean(){
-		
+
+	public void testResultSetWithBoolean() {
+
 		List<BooleanResultSet> booleanResults = new ArrayList<BooleanResultSet>();
 		BooleanResultSet booleanResult1 = new BooleanResultSet(false);
 		booleanResults.add(booleanResult1);
-		
-		String csvResults = beanToCSVReflector.toCSV(booleanResults, new String[]{"endOfTime"});
-		
+
+		String csvResults = beanToCSVReflector.toCSV(booleanResults,
+				new String[] { "endOfTime" });
+
 		assertTrue(csvResults.contains("endOfTime"));
 		assertTrue(csvResults.contains("false"));
 	}
-	
-	private List<ExtendedResultSet> createExtendedTestRow(){
-		ExtendedResultSet extendedResultSet = new ExtendedResultSet("Row 1", new Long(2000), 255, "More info");
+
+	private List<ExtendedResultSet> createExtendedTestRow() {
+		ExtendedResultSet extendedResultSet = new ExtendedResultSet("Row 1",
+				new Long(2000), 255, "More info");
 		List<ExtendedResultSet> list = new ArrayList<ExtendedResultSet>();
 		list.add(extendedResultSet);
 		return list;
 	}
-	
-	
+
 	private List<SimpleResultSet> createSimpleTestRow() {
-		SimpleResultSet simpleResultSet1 = new SimpleResultSet("Row 1", new Long(2000), 255);
+		SimpleResultSet simpleResultSet1 = new SimpleResultSet("Row 1",
+				new Long(2000), 255);
 		List<SimpleResultSet> list = new ArrayList<SimpleResultSet>();
 		list.add(simpleResultSet1);
 		return list;
 	}
-	
-	static class BooleanResultSet{
-		
+
+	static class BooleanResultSet {
+
 		private boolean endOfTime;
 
 		public BooleanResultSet(boolean endOfTime) {
@@ -117,9 +120,9 @@ public class BeanToCSVReflectorTest extends TestCase{
 			this.endOfTime = endOfTime;
 		}
 	}
-	
-	static class SimpleResultSet{
-		
+
+	static class SimpleResultSet {
+
 		private String label;
 		private int number;
 		private Long longValue;
@@ -130,32 +133,38 @@ public class BeanToCSVReflectorTest extends TestCase{
 			this.longValue = longValue;
 			this.number = number;
 		}
-		
+
 		public String getLabel() {
 			return label;
 		}
+
 		public void setLabel(String label) {
 			this.label = label;
 		}
+
 		public int getNumber() {
 			return number;
 		}
+
 		public void setNumber(int number) {
 			this.number = number;
 		}
+
 		public Long getLongValue() {
 			return longValue;
 		}
+
 		public void setLongValue(Long longValue) {
 			this.longValue = longValue;
 		}
 	}
-	
-	static class ExtendedResultSet extends SimpleResultSet{
+
+	static class ExtendedResultSet extends SimpleResultSet {
 
 		public String additionalInfo;
-		
-		public ExtendedResultSet(String label, Long longValue, int number, String addtionalInfo) {
+
+		public ExtendedResultSet(String label, Long longValue, int number,
+				String addtionalInfo) {
 			super(label, longValue, number);
 			this.additionalInfo = addtionalInfo;
 		}
@@ -166,8 +175,7 @@ public class BeanToCSVReflectorTest extends TestCase{
 
 		public void setAdditionalInfo(String additionalInfo) {
 			this.additionalInfo = additionalInfo;
-		}	
+		}
 	}
-	
-	
+
 }
