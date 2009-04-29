bdcbe4adefffdbd58d5f77441461ca155d3d36e3
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-04-29 21:40:38 +0000

    SMS-10. Separate JS for send create-sms.html

---
+++ user-tool/src/webapp/content/js/sms.send.js
@@ -0,0 +1,53 @@
+/**
+ * Library for the send sms page.
+ */
+
+$(document).ready(function(){
+                //$(document).SMS().bind('reveal.facebox', function(){
+					//$.get( "/direct/membership/site/" + $('input[name=sakaiSiteId]').val() + ".json" );
+                   	$.fn.SMS.get.peopleByName(); //Populate the people lists ie:individuals
+                //});
+
+                //if we are reopening a task, focus on messageBody and update char counter
+                $("#messageBody")
+                        .focus()
+                        .click();
+
+                    $("#smsAddRecipients").bind('click',function() {
+                        if($.fn.SMS.get.preserveDomSelections && ! ($("#statusType").val() == "EDIT" || $("#statusType").val() == "REUSE") ){
+                           if ($("#facebox").length != 0) {
+                                $("#facebox").fadeIn('fast');
+                          }
+                        }else{
+                            $.facebox({ajax: this.href});
+                        }
+                        return false;
+                    });
+                //});
+
+                $("#smsSend").bind('click', function(){
+                    //validate variables
+
+                    //Bind EB submit action
+                    $(".loadingImage").show();
+                         var domElements = [ "sakaiUserIds", "deliveryEntityList", "deliveryMobileNumbersSet", "sakaiSiteId", "senderUserName", "senderUserId", "messageBody"];
+                        $.fn.SMS.set.processSubmitTask(domElements, this);
+                        window.location.href = $("#goto-home").attr('href');
+                    });
+
+                if($("#statusType").length != 0 ){
+                    if($("#statusType").val() == "EDIT" || $("#statusType").val() == "REUSE"){
+                        $("#smsSend").removeAttr("disabled");
+                        $.fn.SMS.get.preserveDomSelections = true;
+                    }
+                }
+                /*$("#smsDatesScheduleDate").datepicker({
+                    minDate: 0,
+                    maxDate: '+3M +10D'
+                });
+                $("#smsDatesExpiryDate").datepicker({
+                    minDate: 0,
+                    maxDate: '+3M +10D'
+                });*///$('a[rel=*getReport]').SMS();
+                //jQuery.facebox('some html');
+            });
