082f00e11e121c5a4dbbaa8d6cdb331898cf1339
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-05-13 14:35:15 +0000

    SMS-10. Don't render any nav bar. renderer left inplace for use in phase 2 of the sms tool that may utilise a nav bar.

--- user-tool/src/java/org/sakaiproject/sms/tool/renderers/UserNavBarRenderer.java
+++ user-tool/src/java/org/sakaiproject/sms/tool/renderers/UserNavBarRenderer.java
@@ -13,8 +13,6 @@ import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 
 public class UserNavBarRenderer {
 	
-	private boolean hasCredits = true;
-	
 	/**
 	 * Renders navigation bar
 	 * 
@@ -28,39 +26,9 @@ public class UserNavBarRenderer {
 	 */
 	public void makeNavBar(UIContainer tofill, String divID,
 			String currentViewID) {
-		// TODO: Take permissions into account
-		UIJointContainer joint = new UIJointContainer(tofill, divID,
-				"sms-navigation:");
-
-		renderBranch(joint, "1", currentViewID, MainProducer.VIEW_ID,
-				"sms.navbar.messages", true);
-		if( hasCredits ){
-			renderBranch(joint, "2", currentViewID, SendSMSProducer.VIEW_ID,
-					"sms.navbar.new", false);
-		}
+		// TODO: Phase 2 may have permissions setting that can be rendered here	
 	}
 	public void makeNavBar(UIContainer tofill, String divID,
 			String currentViewID, boolean hasCredits) {
-		this.hasCredits = hasCredits;
-		makeNavBar(tofill, divID, currentViewID);
-	}
-
-	private void renderBranch(UIJointContainer joint, String id,
-			String currentViewID, String linkViewID, String message,
-			boolean renderSeperator) {
-
-		UIBranchContainer cell = UIBranchContainer.make(joint,
-				"navigation-cell:", id);
-
-		if (currentViewID != null && currentViewID.equals(linkViewID)) {
-			UIMessage.make(cell, "item-text", message);
-		} else {
-			UIInternalLink.make(cell, "item-link", UIMessage.make(message),
-					new SimpleViewParameters(linkViewID));
-		}
-
-		if (renderSeperator) {
-			UIOutput.make(cell, "item-separator");
-		}
 	}
 }
