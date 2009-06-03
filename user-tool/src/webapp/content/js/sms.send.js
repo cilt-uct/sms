9edb3205fa2349f548453a2287f20cd7dd6bb4e7
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-06-03 16:13:29 +0000

    SMS-45 Move all onready events to be bound to the facebox afterReveal listener.

--- user-tool/src/webapp/content/js/sms.send.js
+++ user-tool/src/webapp/content/js/sms.send.js
@@ -46,6 +46,8 @@ $(document).ready(function() {
     $("#smsAddRecipients").bind('click', function() {
         if(! $.fn.SMS.set.isRecipientsLinkClicked ){
             if ($.fn.SMS.get.preserveDomSelections && $.fn.SMS.get.selectionsHaveChanged && $("#facebox").length !== 0) {
+                //Stop binding facebox onload events
+                $(document).unbind('afterReveal.facebox');
                 //Keep latest selections in case we want to restore them later on
                 $.fn.SMS.get.previousSelectionsRoles = $.fn.SMS.get.getSelectedRecipientsListIDs("roles");
                 $.fn.SMS.get.previousSelectionsNumbers = $.fn.SMS.get.getSelectedRecipientsListIDs("numbers");
@@ -54,6 +56,10 @@ $(document).ready(function() {
                 $("#recipientsCmd").attr("disabled", "disabled");
                 $("#facebox").fadeIn('fast');
             } else {
+                $(document).unbind('afterReveal.facebox');
+                $(document).bind('afterReveal.facebox', function(){
+                    triggerFaceboxOnloadEvents();
+                });
                 $.facebox({ajax: this.href});
             }
             $.fn.SMS.set.isRecipientsLinkClicked = true;
@@ -95,5 +101,78 @@ $(document).ready(function() {
                 }
             });
         });
+
+    //Move all facebox onload events to  this function to fix SMS-45
+
+    function triggerFaceboxOnloadEvents(){
+        $("#facebox .body .header").text($("#chooseTitle").text());
+        $("#peopleList").tabs();
+
+        //disable tabs that contain no entries in them eg. if there are no groups in site
+        if ( $('#peopleListRoles > div[@rel=Roles] input').length === 0 ){
+            $.fn.SMS.set.disableTab('peopleTabsRoles', 'error-no-roles');
+            }
+        if ( $('#peopleListGroups > div[@rel=Groups] input').length === 0 ){
+            $.fn.SMS.set.disableTab('peopleTabsGroups', 'error-no-groups');
+           }
+
+        $("#peopleListNamesSuggest").autoCompletefb({
+                urlLookup  : $.fn.SMS.get.peopleByName('array'),
+                acOptions  : {
+                    minChars: 1,
+                    matchContains:  true,
+                    selectFirst:    false,
+                    width:  300,
+                    formatItem: function(row) {
+                        return row[0] + ' (' + row[2] + ')';
+                    }
+                },
+                foundClass : ".acfb-data",
+                inputClass : ".acfb-input",
+                deleteImage: $.fn.SMS.settings.images.deleteAutocompleteImage
+        });
+        $("#calculateCmd").bind('click', function(){
+            var domElements = ["sakaiUserIds", "sakaiSiteId","deliveryEntityList", "deliveryMobileNumbersSet"];
+            $.fn.SMS.set.processCalculate(domElements, this);
+        });
+        $("[rel=closeFB]").click(function(){
+            $(document).trigger("close.facebox");
+            $.fn.SMS.get.preserveDomSelections = false;
+            $.fn.SMS.get.preserveNewDomSelections = true;
+            $.fn.SMS.set.isRecipientsLinkClicked = false;
+            return false;
+        });
+        $("#recipientsCmd").bind('click', function(){
+            $("#facebox").hide('fast');
+            $.fn.SMS.set.isRecipientsLinkClicked = false;
+            $.fn.SMS.get.preserveDomSelections = true;
+            $.fn.SMS.set.setSubmitTaskButton(this);
+            //Update billing report
+            $('#reportConsole')
+                    .show()
+                    .html( $('#cReportConsole').html() );
+            //Extract edit text from rel attribute and use that for new link text.
+            $("#smsAddRecipients").text($("#smsAddRecipients").attr("rel").split(",")[1]);
+        });
+       //envoke checkbox bindings
+        $.fn.SMS.get.people();
+       //Re-select saved selections
+        if ($.fn.SMS.get.preserveNewDomSelections){
+            $.fn.SMS.set.restoreSelections();
+        }
+
+        if($("#statusType").length != 0 ){
+            if($("#statusType").val() == "EDIT" || $("#statusType").val() == "REUSE"){
+                $.fn.SMS.get.preserveDomSelections = true;
+                if($("#facebox").length != 0){
+                    $.fn.SMS.set.restoreTaskSelections();
+                }
+            }
+        }
+        //preload small cancel icon. Dimensions are not important
+        var preload1 = new Image(12,12);
+        preload1.src = $.fn.SMS.settings.images.deleteAutocompleteImage;
+
+    }
 
 });
