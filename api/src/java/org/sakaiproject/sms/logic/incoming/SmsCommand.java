6ae8cf791c3b43f54cfa4d755938d7cbdef3929a
David Horwitz <dhorwitz@gmail.com>
2009-07-02 09:52:03 +0000

    SMS-81

--- api/src/java/org/sakaiproject/sms/logic/incoming/SmsCommand.java
+++ api/src/java/org/sakaiproject/sms/logic/incoming/SmsCommand.java
@@ -1,12 +1,15 @@
-/***********************************************************************************
- * SmsCommand.java
- * Copyright (c) 2008 Sakai Project/Sakai Foundation
+/**********************************************************************************
+ * $URL:$
+ * $Id:$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006, 2007 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.osedu.org/licenses/ECL-2.0
+ *       http://www.osedu.org/licenses/ECL-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
