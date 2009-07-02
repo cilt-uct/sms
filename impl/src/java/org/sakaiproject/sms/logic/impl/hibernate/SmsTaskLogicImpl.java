57e739ec425012bc423c2f8338b7cef993121cd3
David Horwitz <dhorwitz@gmail.com>
2009-07-02 10:04:49 +0000

    SMS-81

--- impl/src/java/org/sakaiproject/sms/logic/impl/hibernate/SmsTaskLogicImpl.java
+++ impl/src/java/org/sakaiproject/sms/logic/impl/hibernate/SmsTaskLogicImpl.java
@@ -1,12 +1,15 @@
-/***********************************************************************************
- * SmsTaskLogicImpl.java
- * Copyright (c) 2008 Sakai Project/Sakai Foundation
+/**********************************************************************************
+ * $URL: $
+ * $Id: $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006, 2007 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.osedu.org/licenses/ECL-2.0
+ *       http://www.osedu.org/licenses/ECL-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +18,6 @@
  * limitations under the License.
  *
  **********************************************************************************/
-
 package org.sakaiproject.sms.logic.impl.hibernate;
 
 import java.text.ParseException;
