ac5ba93d4d284185f3d098f1a3e7649b17c237e0
Stephen Marquard <stephen.marquard@uct.ac.za>
2022-01-19 20:47:13 +0200

    VULA-4227 Compiles and passes tests
    Possibly this code is not actually working but may only be used for
    simulator tests.

--- impl/src/java/org/sakaiproject/sms/dao/StandaloneSmsDaoImpl.java
+++ impl/src/java/org/sakaiproject/sms/dao/StandaloneSmsDaoImpl.java
@@ -22,16 +22,25 @@ package org.sakaiproject.sms.dao;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.hbm2ddl.SchemaExport.Action;
 import org.hibernate.tool.hbm2ddl.SchemaValidator;
-import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.hibernate.tool.schema.TargetType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 
+import org.sakaiproject.sms.model.*;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -156,24 +165,22 @@ public class StandaloneSmsDaoImpl extends SmsDaoImpl implements SmsDao {
 		return sessionFactory;
 	}
 
-	public static void sdfsdf() {
-		try {
-			new SchemaValidator(getConfiguration()).validate();
-		} catch (HibernateException e) {
-			log.warn(e.getLocalizedMessage(), e);
-		} catch (IOException e) {
-			log.warn(e.getLocalizedMessage(), e);
-		}
-	}
-
 	/**
 	 * Creates a database schema
 	 */
 	public void createSchema() {
 		try {
-			new SchemaExport(getConfiguration()).create(false, true);
+		  // https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/A-version-5-Hibernate-SchemaExport-example-with-the-ServiceRegistry-and-Metadata
+		  Map<String, String> settings = new HashMap<>();
+	          ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(settings).build();
 
-		} catch (IOException e) {
+		  MetadataSources metadata = new MetadataSources(serviceRegistry);
+		  metadata.addAnnotatedClass(SmsAccount.class);
+
+		  EnumSet<TargetType> enumSet = EnumSet.of(TargetType.DATABASE);
+		  SchemaExport schemaExport = new SchemaExport();
+	          schemaExport.execute(enumSet, Action.BOTH, metadata.buildMetadata());
+		} catch (Exception e) {
 			throw new HibernateException("Error reading hibernate properties: "
 					+ e.getMessage(), e);
 		}
